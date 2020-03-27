6cd4b2938ca6a51a0fe6cf03fa1ab081e36d1377
Volodya <afork4ik@gmail.com>
2020-03-27 10:15:01 +1000

    final

--- LabLogs/Source.cpp
+++ LabLogs/Source.cpp
@@ -9,13 +9,25 @@ int main()
 {
 	int check = 0;
 	string find;
-	cout << "Enter device. (For example: device_ABC): ";
+	cout << "Enter device: ";
 	cin >> find;
 	if (find == "?" || find == "$" || find == "^" || find == "*" || find == "(" || find == ")" || find == ":" || find == "_" || find == "+")
 	{
 		cout << "Error. wut is this symbol?" << endl;
 		system("pause");
 		return 0;
+	}
+	else
+	{
+		string checkFind = find;
+		cmatch result;
+		regex regular("garbage");
+		if (regex_search(checkFind.c_str(), result, regular))
+		{
+			cout << "" << endl;
+			system("pause");
+			return 0;
+		}
 	}
 	for (auto& p : fs::directory_iterator("logs"))
 	{
@@ -25,24 +37,26 @@ int main()
 		{
 			string str = dock;
 			cmatch result;
-			regex regular(find);
+			regex regular("([\\d]+)""(:)""(.+)""(:)""([\\d]+)");
 			if (regex_search(str.c_str(), result, regular))
 			{
-				check++;
-				string path = p.path().string();
-				cmatch resultPath;
-				regex regPath("(log_)""(.)""(.+)""([\\s])""(.+)""([)])""(.txt)");
-				if (regex_search(path.c_str(), resultPath, regPath))
+				if (result[3] == find)
 				{
-					cout << "date: " << resultPath[3] << " time: " << resultPath[5] << "-";
-				}
-				string str = dock;
-				cmatch result;
-				/*regex regular("([\\d]+)""(:)""([a-z]+)""(.+)""(:)""([\\d]+)");*/
-				regex regular("([\\d]+)""(:)""(.+)""(:)""([\\d]+)");
-				if (regex_search(str.c_str(), result, regular))
-				{
-					cout << result[1] << "  " << result[3] << "  " << "result: " << result[5] << endl;
+					check++;
+					string path = p.path().string();
+					cmatch resultPath;
+					regex regPath("(log_)""(.)""(.+)""([\\s])""(.+)""([)])""(.txt)");
+					if (regex_search(path.c_str(), resultPath, regPath))
+					{
+						cout << "date: " << resultPath[3] << " time: " << resultPath[5] << "-";
+					}
+					string str = dock;
+					cmatch result;
+					regex regular("([\\d]+)""(:)""(.+)""(:)""([\\d]+)");
+					if (regex_search(str.c_str(), result, regular))
+					{
+						cout << result[1] << "  " << result[3] << "  " << "result: " << result[5] << endl;
+					}
 				}
 			}
 
